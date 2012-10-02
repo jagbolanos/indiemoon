f814b2e915b2cc29ff021ad0e23e653a2fd5b568
Jorge Garcia <jagbolanos@gmail.com>
2012-10-02 12:51:27 -0600

    empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Indiemoon::Application.config.secret_token = 'c10481366cb6450ff2f5e80c6b096f065c7628e380a1d5a80af59c4c1e5e03da79ee50a0166faa2262fcda5e1decab95c8251f1af6e452dfe499e64bf9b66d90'
